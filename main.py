c9c093d656c1a3081e48e9b41304d9e9acadc91d
zrue <zhjrue@qq.com>
2025-08-20 15:28:28 +0800

    feat: 其余几个合并

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ from api_LLM import class_API, txt_answers_output, find_txts
 
 
 def main(analyzed_text, model_name='qwen-plus-2024-12-20'):
+    """_summary_
+
+    Args:
+        analyzed_text (str): 提示语
+        model_name (str, optional): 设置模型. Defaults to 'qwen-plus-2024-12-20'.
+    """
     txt_fold = "data/txts/"    ##### 设置输入文件夹路径
     sleep_time = 4    ##### 设置延迟时间
 
@@ -55,17 +61,56 @@ def main(analyzed_text, model_name='qwen-plus-2024-12-20'):
         f'延迟耗时{sleep_time * processing_times / 60:.2f}分钟。')
 
 
-# cate_API_plus.py
+
 if __name__ == "__main__":
-    main(
-        analyzed_text='Later, I will show you a chemical literature focused on plastic upcycling. '
-                      'After reading it, please tell me which area or areas this literature is related to. '
-                      'Your answer can only be one or some of the following eleven options: chemistry, engineering, '
-                      'environmental science, materials science, science technology, business economics, energy fuels, '
-                      'polymer science, physics, public environmental occupational health, others. '
-                      'Just give the answer, do not explain your answer. \n\n'
-                      'Here comes to the text: \n\n',
-        model_name='qwen-plus-2024-12-20'    ### 设置模型类型
-    )
+    # # cate_API_plus.py
+    # main(
+    #     analyzed_text='Later, I will show you a chemical literature focused on plastic upcycling. '
+    #                   'After reading it, please tell me which area or areas this literature is related to. '
+    #                   'Your answer can only be one or some of the following eleven options: chemistry, engineering, '
+    #                   'environmental science, materials science, science technology, business economics, energy fuels, '
+    #                   'polymer science, physics, public environmental occupational health, others. '
+    #                   'Just give the answer, do not explain your answer. \n\n'
+    #                   'Here comes to the text:',
+    #     model_name='qwen-plus-2024-12-20'
+    # )
 
+    # # prod_API_max.py
+    # main(
+    #     analyzed_text='Later, I will show you a chemical literature focused on plastic upcycling. '
+    #                   'After reading it, please tell me what kind or kinds of valuable product are obtained in this '
+    #                   'literature. '
+    #                   'Your answer can only be one or some of the following eight options: carbon monoxide, hydrogen, '
+    #                   'hydrocarbons, polymer materials, carbon materials, small organic molecules with oxygen atom, '
+    #                   'monomers of the original plastic, others. '
+    #                   'If the information is not provided or you are unsure, use "N/A" instead. Just give the answer, '
+    #                   'do not explain your answer. \n\n'
+    #                   'Here comes to the text:',
+    #     model_name='qwen-max-2024-09-19' 
+    # )
 
+    # # react_API_max.py
+    # main(
+    #     analyzed_text='Later, I will show you a chemical literature focused on plastic upcycling. '
+    #                   'After reading it, please tell me what kind or kinds of plastic are upcycled in this literature. '
+    #                   'Your answer can only be one or some of the following seven options: polyethylene, polypropylene, '
+    #                   'polyvinyl chloride, polyethylene terephthalate, polystyrene, polyurethane, others. '
+    #                   'If the information is not provided or you are unsure, use "N/A" instead. Just give the answer, '
+    #                   'do not explain your answer. \n\n'
+    #                   'Here comes to the text:',
+    #     model_name='qwen-max-2024-09-19'
+    # )
+
+    # # route_API_max.py
+    # main(
+    #     analyzed_text='Later, I will show you a chemical literature focused on plastic upcycling. '
+    #                  'After reading it, please tell me which category this upcycling process falls into. '
+    #                   'Your answer can only be one of the following six options: mechanical process, chemical process '
+    #                   'with catalyst, chemical process without catalyst, biological process, hybrid process, others. '
+    #                   'If the information is not provided or you are unsure, use "N/A" instead. Just give the answer, '
+    #                   'do not explain your answer. \n\n'
+    #                   'Here comes to the text:',
+    #     model_name='qwen-max-2024-09-19'
+    # )
+
+    main("测试", model_name='qwen-plus-2024-12-20')
